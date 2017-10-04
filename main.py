e76bb97085a75f215243fd97b134fa093242e7d1
Jeff <jffcole7@gmail.com>
2017-10-04 06:50:49 -0400

    one field

--- main.py
+++ main.py
@@ -3,28 +3,36 @@ import requests as rq
 import random
 
 
-while True:
-    num = random.randint(2000000,3000000)
-
-    bugguide = "http://bugguide.net/node/view/"+str(num)+"/bgimage" 
-    # bugguide ="http://bugguide.net/node/view/1446859/bgimage"
-
+for num in range(2000000):
+    bugguide = "http://bugguide.net/node/view/"+str(num)+"/bgimage"
     page = rq.get(bugguide)
     contents = page.content
-
-    # print(contents)
-
     soup = bs(contents,'html.parser')
-
-    # print(soup.find_all('img',"bgimage-image"))
     bug_pic = soup.find_all('img',"bgimage-image")
-    data = {}
     # print(bug_pic)
     try:
 
-        print(bug_pic[0].get("title"))
+        title=bug_pic[0].get("title")
+        src=bug_pic[0].get("src")
+        titleSplit = title.split(' - ')
+        titleFields = len(titleSplit)
+
+        if titleFields == 1:
+            title2write = "wget -O bg_"+str(num) +'_'+title.replace(" ", "-")+".jpeg "+src
+        # elif titleFields == 2:
+        #     title2write = titleSplit[1]
+        # elif "female" not in titleFields[:-1] and "male" not in titleFields[:-1]:
+        #     title2write = titleFields[:-1]
+        # else:
+        #     print("FUCK: ",title)
+        print(title2write)
+
+        src=bug_pic[0].get("src")
+        # print(len(title.split(' - ' )),title)
     except:
-        print(bugguide," doesn't exist")
+        None
+    # print(num)
+        # print(bugguide," doesn't exist")
     # usedBefore[num] =True
     # title = a.string.strip()
     # print(a)
